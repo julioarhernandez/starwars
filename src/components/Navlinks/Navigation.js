c90624731fc901bff232e3f6492f0d7712e6268b
Julio Rodriguez <juliorodriguez@ncl.com>
2020-02-22 22:01:01 -0500

    Navigation Class for GSAP

--- src/components/Navlinks/Navigation.js
+++ src/components/Navlinks/Navigation.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {TweenLite} from "gsap";
 import {NavLink} from "react-router-dom";
+
 import './Navigation.scss';
 
-const Navigation = (props) => {
-    return (
-        <>
-            <NavLink exact activeClassName="active" to="/">Home</NavLink>
-            <NavLink activeClassName="active" to={'/persons/'}>Persons</NavLink>
-            <NavLink activeClassName="active" to={'/films/'}>Films</NavLink>
-        </>
-    );
+class Navigation extends Component {
+    constructor() {
+        super();
+        this.nav =[];
+    }
+
+    componentDidMount() {
+        TweenLite.from(this.nav, 0.3  , {y: -100, stagger: 0.2});
+    }
+
+    render(){
+        return(
+            <div className="Navigation">
+                <NavLink exact activeClassName="active" to="/" ref={div => this.nav[0] = div}>Home</NavLink>
+                <NavLink activeClassName="active" to={'/persons/'} ref={div => this.nav[1] = div}>Persons</NavLink>
+                <NavLink activeClassName="active" to={'/films/'} ref={div => this.nav[2] = div}>Films</NavLink>
+            </div>
+        )
+    }
 }
 
 export default Navigation;
