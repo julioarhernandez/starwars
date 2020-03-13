37deb7ad04908ec53152ba60e03c8b2e9393c55c
Julio <Julioarhernandez@gmail.com>
2020-03-13 00:10:14 -0400

    Home effects

--- src/components/Home/Home-view.js
+++ src/components/Home/Home-view.js
@@ -1,19 +1,28 @@
-import React from "react";
-import logo from "../../images/SW.svg";
+import React, {useEffect, useRef, useState} from "react";
+import {TimelineMax, TweenMax} from "gsap";
+import stars from "../../images/stars.svg";
+import wars from "../../images/stars.svg";
 import "./Home.scss";
 
 const HomeView = () => {
+    const starsLogo = useRef();
+    const warsLogo = useRef();
+    const [tl] = useState(new TimelineMax({ paused: true }));
 
-  return (
-    <div className="Home">
-      <div className="Home_image">
-        <img src={logo} className="Home_logo" alt="SW logo"/>
-      </div>
-        <div className="Home_text">
-            The API Awakens
+    useEffect(() => {
+        TweenMax.to(starsLogo.current, 0.5, {y:10}).to(warsLogo.current, 0.5, {y:20}).pause();
+    }, [tl]);
+    return (
+        <div className="Home">
+            <div className="Home_image">
+                <img src={stars} className="Home_logo-stars" alt="SW logo" ref={el => starsLogo.current = el }/>
+                <img src={wars} className="Home_logo-wars" alt="SW logo" ref={el => warsLogo.current = el }/>
+            </div>
+            <div className="Home_text">
+                The API Awakens
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default HomeView;
